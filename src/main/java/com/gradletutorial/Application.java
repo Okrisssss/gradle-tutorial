821532a8ba24e97690df06b4795e1a3bcbb1b8d6
piachimov <petru.iachimov@endava.com>
2020-04-10 12:34:51 +0300

    first commit

---
+++ src/main/java/com/gradletutorial/Application.java
@@ -0,0 +1,8 @@
+package com.gradletutorial;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Gtadle from Application Class!");
+    }
+}
